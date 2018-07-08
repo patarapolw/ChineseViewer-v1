2156bc56ad2277bc41e6feaaba482abab00cfc78
patarapolw <patarapolw@gmail.com>
2018-07-08 14:05:11 +0700

    Use gTTS directly, instead of flask_gtts

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_gtts import gtts
 
 from webapp.blueprints import blueprint
 
 app = Flask(__name__)
-gtts(app, route=True)
 app.register_blueprint(blueprint)
 
 from webapp.views import hanzi, utils, vocab, item
